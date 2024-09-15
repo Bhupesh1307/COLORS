1e245e2e31545e1ceae4a7c511688798a3817399
Bhupesh <125125508+Bhupesh1307@users.noreply.github.com>
2024-09-15 13:55:12 +0530

    first commit

---
+++ COLORS.bat
@@ -0,0 +1,161 @@
+@echo off
+title Colors	
+cls
+echo.
+echo.
+echo				     [94m________  ________  ___       ________  ________  ________[0m      
+echo				    [94m^|\   ____\^|\   __  \^|\  \     ^|\   __  \^|\   __  \^|\   ____\[0m     
+echo				    [94m\ \  \___^|\ \  \^|\  \ \  \    \ \  \^|\  \ \  \^|\  \ \  \___^|_[0m    
+echo	 			     [94m\ \  \    \ \  \\\  \ \  \    \ \  \\\  \ \   _  _\ \_____  \[0m   
+echo	 			      [94m\ \  \____\ \  \\\  \ \  \____\ \  \\\  \ \  \\  \\^|____^|\  \[0m  
+echo	 			       [94m\ \_______\ \_______\ \_______\ \_______\ \__\\ _\ ____\_\  \[0m 
+echo					[94m\^|_______^|\^|_______^|\^|_______^|\^|_______^|\^|__^|\^|__^|\_________\[0m
+echo			    	                                                         [94m\^|_________^|[0m
+                                                                 
+                                                                 
+echo.
+echo						    [95mFormat text in Batch files like a pro![0m
+echo								[38;5;12mVersion: 1.0.0[0m
+echo.                                                  
+timeout /t 3 > nul                                                                                                                                
+echo.
+echo.
+echo 						   [104;93m Basic Color Options [0m
+echo.
+echo Use "color [background][foreground]" command to set a specific color to your console.
+echo    0 = Black       8 = Gray
+echo    1 = Blue        9 = Light Blue
+echo    2 = Green       A = Light Green
+echo    3 = Aqua        B = Light Aqua
+echo    4 = Red         C = Light Red
+echo    5 = Purple      D = Light Purple
+echo    6 = Yellow      E = Light Yellow
+echo    7 = White       F = Bright White
+echo.
+echo.
+echo 						   [104;93m Advanced Formatting [0m
+echo.
+echo.
+echo [101;93m STYLES [0m
+echo ^<ESC^>[0m [0mReset[0m
+echo ^<ESC^>[1m [1mBold[0m
+echo ^<ESC^>[4m [4mUnderline[0m
+echo ^<ESC^>[7m [7mInverse[0m
+echo.
+echo [101;93m NORMAL FOREGROUND COLORS [0m
+echo ^<ESC^>[30m [30mBlack[0m (black)
+echo ^<ESC^>[31m [31mRed[0m
+echo ^<ESC^>[32m [32mGreen[0m
+echo ^<ESC^>[33m [33mYellow[0m
+echo ^<ESC^>[34m [34mBlue[0m
+echo ^<ESC^>[35m [35mMagenta[0m
+echo ^<ESC^>[36m [36mCyan[0m
+echo ^<ESC^>[37m [37mWhite[0m
+echo.
+echo [101;93m NORMAL BACKGROUND COLORS [0m
+echo ^<ESC^>[40m [40mBlack[0m
+echo ^<ESC^>[41m [41mRed[0m
+echo ^<ESC^>[42m [42mGreen[0m
+echo ^<ESC^>[43m [43mYellow[0m
+echo ^<ESC^>[44m [44mBlue[0m
+echo ^<ESC^>[45m [45mMagenta[0m
+echo ^<ESC^>[46m [46mCyan[0m
+echo ^<ESC^>[47m [47mWhite[0m (white)
+echo.
+echo [101;93m STRONG FOREGROUND COLORS [0m
+echo ^<ESC^>[90m [90mWhite[0m
+echo ^<ESC^>[91m [91mRed[0m
+echo ^<ESC^>[92m [92mGreen[0m
+echo ^<ESC^>[93m [93mYellow[0m
+echo ^<ESC^>[94m [94mBlue[0m
+echo ^<ESC^>[95m [95mMagenta[0m
+echo ^<ESC^>[96m [96mCyan[0m
+echo ^<ESC^>[97m [97mWhite[0m
+echo.
+echo [101;93m STRONG BACKGROUND COLORS [0m
+echo ^<ESC^>[100m [100mBlack[0m
+echo ^<ESC^>[101m [101mRed[0m
+echo ^<ESC^>[102m [102mGreen[0m
+echo ^<ESC^>[103m [103mYellow[0m
+echo ^<ESC^>[104m [104mBlue[0m
+echo ^<ESC^>[105m [105mMagenta[0m
+echo ^<ESC^>[106m [106mCyan[0m
+echo ^<ESC^>[107m [107mWhite[0m
+echo.
+echo [101;93m EXTENDED FOREGROUND COLORS [0m
+echo ^<ESC^>[38;5;0m [38;5;0mBlack[0m (black)
+echo ^<ESC^>[38;5;1m [38;5;1mRed[0m
+echo ^<ESC^>[38;5;2m [38;5;2mGreen[0m
+echo ^<ESC^>[38;5;3m [38;5;3mYellow[0m
+echo ^<ESC^>[38;5;4m [38;5;4mBlue[0m
+echo ^<ESC^>[38;5;5m [38;5;5mMagenta[0m
+echo ^<ESC^>[38;5;6m [38;5;6mCyan[0m
+echo ^<ESC^>[38;5;7m [38;5;7mWhite[0m
+echo ^<ESC^>[38;5;8m [38;5;8mGray[0m
+echo ^<ESC^>[38;5;9m [38;5;9mBright Red[0m
+echo ^<ESC^>[38;5;10m [38;5;10mBright Green[0m
+echo ^<ESC^>[38;5;11m [38;5;11mBright Yellow[0m
+echo ^<ESC^>[38;5;12m [38;5;12mBright Blue[0m
+echo ^<ESC^>[38;5;13m [38;5;13mBright Magenta[0m
+echo ^<ESC^>[38;5;14m [38;5;14mBright Cyan[0m
+echo ^<ESC^>[38;5;15m [38;5;15mBright White[0m
+echo ^<ESC^>[38;5;208m [38;5;208mOrange[0m
+echo ^<ESC^>[38;5;199m [38;5;199mHot Pink[0m
+echo ^<ESC^>[38;5;118m [38;5;118mLime[0m
+echo ^<ESC^>[38;5;220m [38;5;220mGold[0m
+echo ^<ESC^>[38;5;17m [38;5;17mDark Blue[0m
+echo ^<ESC^>[38;5;148m [38;5;148mPeach[0m
+echo ^<ESC^>[38;5;54m [38;5;54mPurple[0m
+echo ^<ESC^>[38;5;13m [38;5;13mLight Pink[0m
+echo ^<ESC^>[38;5;237m [38;5;237mDark Gray[0m
+echo ^<ESC^>[38;5;48m [38;5;48mSea Green[0m
+echo ^<ESC^>[38;5;93m [38;5;93mViolet[0m
+echo ^<ESC^>[38;5;21m [38;5;21mElectric Blue[0m
+echo ^<ESC^>[38;5;68m [38;5;68mTeal[0m
+echo ^<ESC^>[38;5;106m [38;5;106mTurquoise[0m
+echo ^<ESC^>[38;5;162m [38;5;162mCoral[0m
+echo.
+echo [101;93m EXTENDED BACKGROUND COLORS [0m
+echo ^<ESC^>[48;5;0m [48;5;0mBlack[0m
+echo ^<ESC^>[48;5;1m [48;5;1mRed[0m
+echo ^<ESC^>[48;5;2m [48;5;2mGreen[0m
+echo ^<ESC^>[48;5;3m [48;5;3mYellow[0m
+echo ^<ESC^>[48;5;4m [48;5;4mBlue[0m
+echo ^<ESC^>[48;5;5m [48;5;5mMagenta[0m
+echo ^<ESC^>[48;5;6m [48;5;6mCyan[0m
+echo ^<ESC^>[48;5;7m [48;5;7mWhite[0m (white)
+echo ^<ESC^>[48;5;8m [48;5;8mGray[0m
+echo ^<ESC^>[48;5;9m [48;5;9mBright Red[0m
+echo ^<ESC^>[48;5;10m [48;5;10mBright Green[0m
+echo ^<ESC^>[48;5;11m [48;5;11mBright Yellow[0m
+echo ^<ESC^>[48;5;12m [48;5;12mBright Blue[0m
+echo ^<ESC^>[48;5;13m [48;5;13mBright Magenta[0m
+echo ^<ESC^>[48;5;14m [48;5;14mBright Cyan[0m
+echo ^<ESC^>[48;5;15m [48;5;15mBright White[0m
+echo ^<ESC^>[48;5;208m [48;5;208mOrange[0m
+echo ^<ESC^>[48;5;199m [48;5;199mHot Pink[0m
+echo ^<ESC^>[48;5;118m [48;5;118mLime[0m
+echo ^<ESC^>[48;5;220m [48;5;220mGold[0m
+echo ^<ESC^>[48;5;17m [48;5;17mDark Blue[0m
+echo ^<ESC^>[48;5;130m [48;5;130mBrown[0m
+echo ^<ESC^>[48;5;196m [48;5;196mCrimson Red[0m
+echo ^<ESC^>[48;5;148m [48;5;148mPeach[0m
+echo ^<ESC^>[48;5;54m [48;5;54mPurple[0m
+echo ^<ESC^>[48;5;13m [48;5;13mLight Pink[0m
+echo ^<ESC^>[48;5;237m [48;5;237mDark Gray[0m
+echo ^<ESC^>[48;5;48m [48;5;48mSea Green[0m
+echo ^<ESC^>[48;5;93m [48;5;93mViolet[0m
+echo ^<ESC^>[48;5;21m [48;5;21mElectric Blue[0m
+echo ^<ESC^>[48;5;68m [48;5;68mTeal[0m
+echo ^<ESC^>[48;5;106m [48;5;106mTurquoise[0m
+echo ^<ESC^>[48;5;162m [48;5;162mCoral[0m
+echo.
+echo ^[101;93m COMBINATIONS [0m
+echo ^<ESC^>[31m                     [31mred foreground color[0m
+echo ^<ESC^>[7m                      [7minverse foreground ^<-^> background[0m
+echo ^<ESC^>[7;31m                   [7;31minverse red foreground color[0m
+echo ^<ESC^>[7m and nested ^<ESC^>[31m [7mbefore [31mnested[0m
+echo ^<ESC^>[31m and nested ^<ESC^>[7m [31mbefore [7mnested[0m
+echo.
+echo Press any key to Exit...
+timeout /t -1 > nul
